e84f1012728e9d32b60c398a71821d1fd66e0ff3
marfino3028 <marfinohamzah455@gmail.com>
2020-10-25 20:50:24 +0700

    simoneb

--- app/Http/Controllers/KaryaController.php
+++ app/Http/Controllers/KaryaController.php
@@ -34,7 +34,8 @@ class KaryaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $karya = Karya::get();
+        return view('upload', ['gambar' => $gambar]);
     }
 
     /**
@@ -56,7 +57,28 @@ class KaryaController extends Controller
      */
     public function edit($id)
     {
-        //
+        
+        $this->validate($request, [
+            'file' => 'required|file|image|mimes:jpeg,png,jpg|max:2048',
+            'keterangan' => 'required',
+        ]);
+
+        // menyimpan data file yang diupload ke variabel $file
+        $file = $request->file('file');
+        // nama file
+        $nama_file = time() . "_" . $file->getClientOriginalName();
+//        // isi dengan nama folder tempat kemana file diupload
+        $tujuan_upload = 'data_file';
+        $file->move($tujuan_upload, $nama_file);
+
+        // upload file
+//        $file->move($tujuan_upload, $file->getClientOriginalName());
+        Karya::create([
+            'file' => $nama_file,
+            'keterangan' => $request->keterangan,
+        ]);
+
+        return redirect()->back();
     }
 
     /**
